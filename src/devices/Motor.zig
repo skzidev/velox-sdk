eed5a58b3cdae2b566fe629f7162774ca1332258
skzidev <chervonijpes@gmail.com>
2026-08-18 12:39:35 -0400

    added some more motor functions

--- src/devices/Motor.zig
+++ src/devices/Motor.zig
@@ -1,18 +1,45 @@
 const jmptbl = @import("velox_jumptable");
 const units = @import("../units.zig");
+const errors = @import("../error.zig");
 
-pub const cartridges = enum {
-    red,
-    blue,
+/// An enum representing the possible, official VEX motor cartridges
+pub const MotorCartridge = enum(c_int) {
+    /// ## 36:1
+    red = 0,
+    /// ## 18:1
     green,
+    /// ## 6:1
+    blue,
+    _,
+};
+
+/// An enum representing the V5 motor braking modes
+pub const BrakeMode = enum(c_int) {
+    // this MUST BE the same as in the jumptable
+    /// ## Coast Mode
+    /// Provide no active braking.
+    ///
+    /// The motor will be naturally slowed down by friction
+    coast = 0,
+    /// ## Brake Mode
+    /// Provide active braking, but do not hold the motor in its place.
+    ///
+    brake,
+    /// ## Hold mode
+    /// Lock the motor in place where it is.
+    ///
+    hold,
+    _,
 };
 
 /// Represents a VEX V5 Motor (Both 11w and 5.5w)
 pub const Motor = struct {
-    const _handle: ?*anyopaque = null;
+    _handle: ?*anyopaque = null,
     /// Defines whether this motor is reversed or not
     /// This can be overrided at runtime.
-    pub var isReversed: bool = false;
+    isReversed: bool = false,
+    //// The motor cartridge that is inserted into the motor
+    cartridge: MotorCartridge,
     /// Spins the motor at an specified number of specified units.
     ///
     /// The sign of `scalar` controls which direction it spins in. If it is positive, it spins forward, otherwise backward.
@@ -32,7 +59,7 @@ pub const Motor = struct {
         /// The units which the value is in.
         unit: units.MotorUnit,
     ) void {
-        if (isReversed) scalar *= -1;
+        scalar = if (self.isReversed) -scalar else scalar;
         switch (unit) {
             .rpm => {
                 jmptbl.motor.vexDeviceMotorVelocitySet(self._handle, scalar);
@@ -41,13 +68,32 @@ pub const Motor = struct {
                 jmptbl.motor.vexDeviceMotorVoltageSet(self._handle, scalar);
             },
             .percent => {
-                jmptbl.motor.vexDeviceMotorVoltageSet(self._handle, (scalar / 100) * 127);
+                jmptbl.motor.vexDeviceMotorVoltageSet(self._handle, (scalar * 127) / 100);
             },
         }
     }
 
+    fn init(
+        /// The port # of the device
+        port: i8,
+        /// Whether the motor shold be reversed
+        reversed: bool,
+        /// The motor cartridge.
+        cart: MotorCartridge,
+    ) errors.DeviceInitError!Motor {
+        if (port < -21 or port > 21 or port == 0)
+            return errors.DeviceError.InvalidPortError;
+        const handle = jmptbl.devices.vexDeviceGetByIndex(port - 1);
+        return Motor{
+            ._handle = handle,
+            .isReversed = reversed,
+            .cartridge = cart,
+        };
+    }
+
     /// Get the temperature of the motor in the specified units
     ///
+    /// **Return Value**: The temperature in the specified units.
     pub fn temp(
         self: *Motor,
         /// The units in which the response should be returned
@@ -58,5 +104,15 @@ pub const Motor = struct {
             .celsius => cTemp,
             .farenheit => (cTemp * (9 / 5)) + 32,
         };
+    }
+
+    /// Returns a boolean flag which tells if the motor is overheating
+    pub fn isOverheating(self: *Motor) bool {
+        return jmptbl.motor.vexDeviceMotorOverTempFlagGet(self._handle);
+    }
+
+    /// Sets the braking mode on the motor
+    pub fn setBrakingMode(self: *Motor, mode: BrakeMode) void {
+        jmptbl.motor.vexDeviceMotorBrakeModeSet(self._handle, mode);
     }
 };
---
+++ src/error.zig
@@ -0,0 +1,3 @@
+pub const DeviceInitError = error{
+    InvalidPortError,
+};
