a0664f5511786731e3677bd0765e833e1f2e3e17
skzidev <chervonijpes@gmail.com>
2026-08-19 08:39:52 -0400

    Add more rotational sensor functions

--- src/devices/Rotation.zig
+++ src/devices/Rotation.zig
@@ -1,4 +1,6 @@
 const jmptbl = @import("velox_jumptable");
+const units = @import("../units.zig");
+const pi = @import("std").math.pi;
 
 pub const Rotation = struct {
     _handle: ?*anyopaque,
@@ -11,8 +13,42 @@ pub const Rotation = struct {
         };
     }
 
+    pub fn reset(self: *Rotation) void {
+        jmptbl.rotation.vexDeviceAbsEncReset(self._handle);
+    }
+
+    pub fn velocity(self: *Rotation) i32 {
+        // TODO add units
+        return jmptbl.rotation.vexDeviceAbsEncVelocityGet(self._handle);
+    }
+
+    pub fn isReversed(self: *Rotation) bool {
+        return jmptbl.rotation.vexDeviceAbsEncReverseFlagGet(self._handle);
+    }
+
+    pub fn setReversed(self: *Rotation, reversed: bool) void {
+        jmptbl.rotation.vexDeviceAbsEncReverseFlagSet(self._handle, reversed);
+    }
+
     /// TODO add units
     pub fn pos(self: *Rotation) i32 {
         return jmptbl.rotation.vexDeviceAbsEncPositionGet(self._handle);
+    }
+
+    pub fn setPos(self: *Rotation, value: i32) void {
+        jmptbl.rotation.vexDeviceAbsEncPositionSet(self._handle, value);
+    }
+
+    pub fn angle(self: *Rotation, unit: units.RotationalUnit) i32 {
+        const deg = jmptbl.rotation.vexDeviceAbsEncAngleGet(self._handle);
+        return switch (unit) {
+            .radian => deg * (pi / 180),
+            .degree => deg,
+            .turn => deg / 360,
+        };
+    }
+
+    pub fn setDataRate(self: *Rotation, rate: u32) void {
+        return jmptbl.rotation.vexDeviceAbsEncDataRateSet(self._handle, rate);
     }
 };
